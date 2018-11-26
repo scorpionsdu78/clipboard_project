ed196557f77bdb496eee519c5ff85facc6d5cd5c
scorpionsdu78 <fboni.78@gmail.com>
2018-11-26 22:17:33 +0100

    last changes

--- elementString.java
+++ elementString.java
@@ -3,18 +3,15 @@ package clipboard_project;
 public class elementString extends element  {
 
 	private String content;
-	
+
 	public elementString(String input)
 	{
 		content = input;
 	}
-	
-	
-	/*public String getContent() {
-		// TODO Auto-generated method stub
-		return content;
-	}*/
-	
+
+
+
+
 	public void display()
 	{
 		System.out.println(content);
