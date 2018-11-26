864e2db73ddb49176f256275e7ad0aea0bfa22ec
scorpionsdu78 <fboni.78@gmail.com>
2018-11-26 22:24:35 +0100

    last changes

--- clipboard.java
+++ clipboard.java
@@ -4,18 +4,18 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class clipboard {
-	
+
 	List<element> elements;
 	int size;
 	int nb_element;
-	
+
 	public clipboard(int i_size)
 	{
 		size = i_size;
 		nb_element=0;
 		elements = new LinkedList<element>();
 	}
-	
+
 	public void add_element(int input)
 	{
 		if(nb_element < size)
@@ -23,16 +23,16 @@ public class clipboard {
 			elements.add(new elementInt(input));
 			nb_element++;
 		}
-		
+
 		else
 		{
 			elements.add(new elementInt(input));
 			elements.remove(0);
-			
+
 		}
-		
+
 	}
-	
+
 	public void add_element(double input)
 	{
 		if(nb_element < size)
@@ -40,16 +40,16 @@ public class clipboard {
 			elements.add(new elementfloat(input));
 			nb_element++;
 		}
-		
+
 		else
 		{
 			elements.add(new elementfloat(input));
 			elements.remove(0);
-			
+
 		}
-		
+
 	}
-	
+
 	public void add_element(String input)
 	{
 		if(nb_element < size)
@@ -57,18 +57,18 @@ public class clipboard {
 			elements.add(new elementString(input));
 			nb_element++;
 		}
-		
+
 		else
 		{
 			elements.add(new elementString(input));
 			elements.remove(0);
-			
+
 		}
-		
-		
-		
+
+
+
 	}
-	
+
 	public void display()
 	{
 		for(element e : elements)
@@ -76,37 +76,42 @@ public class clipboard {
 			e.display();
 		}
 	}
-	
+
 	public int getNumberCount()
 	{
 		int count=0;
-		
+
 		for(element e : elements)
 		{
 			if(e.isNumber())
 				count ++;
 		}
-		
+
 		return count;
-		
+
 	}
-	
+
 	public double getNumberSum()
 	{
 		double sum=0;
-		
+
 		for(element e : elements)
 		{
 			if(e.isNumber())
 			{
 				elementnumber a = (elementnumber) e;
 				sum = sum + a.getContent();
-				
+
 			}
 		}
-		
+
 		return sum;
-		
+
+	}
+
+	public int gitItemCount()
+	{
+	    return nb_element;
 	}
 
 }
